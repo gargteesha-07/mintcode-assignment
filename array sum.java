c707dd79bbe293065746cb9f867c88815b5e4045
Teesha Garg <gargteesha2107@gmail.com>
2025-04-22 23:21:45 +0530

    Update array sum.java

--- array sum.java
+++ array sum.java
@@ -2,11 +2,9 @@ public class SumOfArray {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5}; 
         int sum = 0;
-
-        for (int i = 0; i < array.length; i++) {
+for (int i = 0; i < array.length; i++) {
             sum += array[i];
-        }
-
-        System.out.println("Sum of array elements: " + sum);
+}
+ System.out.println("Sum of array elements: " + sum);
     }
 }
