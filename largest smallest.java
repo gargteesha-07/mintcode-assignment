3cebba2f7085cb45092e8c0868f47afab7f3255b
Teesha Garg <gargteesha2107@gmail.com>
2025-04-22 23:17:45 +0530

    Create largest smallest.java

---
+++ largest smallest.java
@@ -0,0 +1,21 @@
+public class LargestSmallest {
+
+    public static void main(String[] args) {
+        int[] array = {10, 25, 5, 17, 40, 2};
+
+        int largest = array[0];
+        int smallest = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+        System.out.println("Smallest: " + smallest);
+    }
+}
