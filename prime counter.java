07bda66e5d9d3e18c06695f02586ce260e758faf
Teesha Garg <gargteesha2107@gmail.com>
2025-04-22 23:23:37 +0530

    Update prime counter.java

--- prime counter.java
+++ prime counter.java
@@ -1,26 +1,21 @@
 public class PrimeCounter {
-
-    public static boolean isPrime(int num) {
+ public static boolean isPrime(int num) {
         if (num <= 1) return false;
         if (num == 2) return true;
         if (num % 2 == 0) return false;
-        
-        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+         for (int i = 3; i <= Math.sqrt(num); i += 2) {
             if (num % i == 0) return false;
         }
         return true;
     }
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         int[] array = {2, 3, 4, 5, 6, 7, 9};
         int primeCount = 0;
-
-        for (int num : array) {
+ for (int num : array) {
             if (isPrime(num)) {
                 primeCount++;
             }
         }
-
-        System.out.println("Number of prime numbers: " + primeCount);
+System.out.println("Number of prime numbers: " + primeCount);
     }
 }
