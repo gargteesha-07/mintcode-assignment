2565ed5826d81a251ce3be018ff90a1ca84ee477
Teesha Garg <gargteesha2107@gmail.com>
2025-04-22 23:38:13 +0530

    Update largest smallest.java

--- largest smallest.java
+++ largest smallest.java
@@ -1,17 +1,29 @@
-public class LargestSmallest {
- public static void main(String[] args) {
-        int[] array = {10, 25, 5, 17, 40, 2};
- int largest = array[0];
-        int smallest = array[0];
-for (int i = 1; i < array.length; i++) {
-            if (array[i] > largest) {
-                largest = array[i];
-            }
-            if (array[i] < smallest) {
-                smallest = array[i];
+public class smallestlargest {
+    public static int findLargest(int[] arr){
+        int largest = arr[0];
+        for(int num : arr){
+            if(num>largest){
+                largest=num;
             }
         }
- System.out.println("Largest: " + largest);
-        System.out.println("Smallest: " + smallest);
+        return largest;
+    }
+    public static int findSmallest(int [] arr){
+        int smallest = arr[0];
+        for(int num: arr){
+            if(num<smallest){
+                smallest=num;
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args) {
+        int[] array ={10,25,5,17,40,2};
+
+        int largest = findLargest(array);
+        int smallest = findSmallest(array);
+
+        System.out.println("Largest: " +largest);
+        System.out.println("Smallest: " +smallest);
     }
 }
